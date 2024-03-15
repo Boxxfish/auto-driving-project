2cbf12c3c494de988b8822897ca8c03991bba8fd
John Mihal <johnmihal34@gmail.com>
2024-03-14 18:19:04 -0700

    added c++ file

---
+++ visualize.cpp
@@ -0,0 +1,14 @@
+// C++ program to illustrate the use
+// of cout object
+#include <iostream>
+using namespace std;
+
+// Driver Code
+int main()
+{
+	// Print standard output
+	// on the screen
+	cout << "HIII";
+
+	return 0;
+}

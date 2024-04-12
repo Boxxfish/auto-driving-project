93ada505226a538b574cbc98f459968ce3b2a8dc
John Mihal <johnmihal34@gmail.com>
2024-04-12 01:43:51 -0400

    trying to get rotation matrix to work

--- ICP C++/icp.cpp
+++ ICP C++/icp.cpp
@@ -25,13 +25,10 @@ struct Rotation {
     double pitch, yaw, roll;
 };
 
-void printMatrix(const std::string& name, const Eigen::Matrix4d& matrix) {
-    std::cout << name << ":\n" << matrix << "\n\n";
-}
 
 int parseLine(const std::string& line, Location& loc, Rotation& rot) {
-  std::cout << "in parse line" << std::endl;
-  std::cout << line << std::endl;
+  // std::cout << "in parse line" << std::endl;
+  // std::cout << line << std::endl;
 
   std::istringstream iss(line);
   std::string token;
@@ -41,97 +38,138 @@ int parseLine(const std::string& line, Location& loc, Rotation& rot) {
   // Get frame num
   iss >> token;
   frame_num = stoi(token);
-  std::cout << frame_num << std::endl;
+  // std::cout << frame_num << std::endl;
 
   //ex token = "Transform(Location(x=-112.302399,"
   iss >> token;
-  std::cout << token << std::endl;
+  // std::cout << token << std::endl;
   std::string delimiter_start = "=";
   std::string delimiter_end = ",";
   token = token.substr(token.find(delimiter_start)+1, token.find(delimiter_end)-token.find(delimiter_start));
   loc.x = stod(token);
-  std::cout << loc.x << std::endl;
+  // std::cout << loc.x << std::endl;
 
   //ex token = "y=2.822677,"
   iss >> token;
-  std::cout << token << std::endl;
+  // std::cout << token << std::endl;
   delimiter_start = "=";
   delimiter_end = ",";
   token = token.substr(token.find(delimiter_start)+1, token.find(delimiter_end)-token.find(delimiter_start));
   loc.y = stod(token);
-  std::cout << loc.y << std::endl;
+  // std::cout << loc.y << std::endl;
 
   //ex token = "z=3.647835),"
   iss >> token;
-  std::cout << token << std::endl;
+  // std::cout << token << std::endl;
   delimiter_start = "=";
   delimiter_end = ")";
   token = token.substr(token.find(delimiter_start)+1, token.find(delimiter_end)-token.find(delimiter_start));
   loc.z = stod(token);
-  std::cout << loc.z << std::endl;
+  // std::cout << loc.z << std::endl;
 
   //ex token = "Rotation(pitch=0.000000,"
   iss >> token;
-  std::cout << token << std::endl;
+  // std::cout << token << std::endl;
   delimiter_start = "=";
   delimiter_end = ",";
   token = token.substr(token.find(delimiter_start)+1, token.find(delimiter_end)-token.find(delimiter_start));
   rot.pitch = stod(token);
-  std::cout << rot.pitch << std::endl;
+  // std::cout << rot.pitch << std::endl;
 
 
   //ex token = "yaw=-0.139465,"
   iss >> token;
-  std::cout << token << std::endl;
+  // std::cout << token << std::endl;
   delimiter_start = "=";
   delimiter_end = ",";
   token = token.substr(token.find(delimiter_start)+1, token.find(delimiter_end)-token.find(delimiter_start));
   rot.yaw = stod(token);
-  std::cout << rot.yaw << std::endl;
+  // std::cout << rot.yaw << std::endl;
 
   //ex token = "roll=0.000000))"
   iss >> token;
-  std::cout << token << std::endl;
+  // std::cout << token << std::endl;
   delimiter_start = "=";
   delimiter_end = ")";
   token = token.substr(token.find(delimiter_start)+1, token.find(delimiter_end)-token.find(delimiter_start));
   rot.roll = stod(token);
-  std::cout << rot.roll << std::endl;
+  // std::cout << rot.roll << std::endl;
 
   return frame_num;
 }
 
-void generate_matrix(){
-  std::ifstream file("../../splits/pose_c.txt");
+void generate_matrix(Eigen::Matrix4d& translation_matrix, const std::string& path, int data_num){
+  std::ifstream file(path);
   std::string line;
   std::cout << "in generate matrix" << std::endl;
 
   while (std::getline(file, line)) {
-      std::cout << "reading a new line" << std::endl;
-      Location loc;
-      Rotation rot;
-      parseLine(line, loc, rot);
+    // std::cout << "reading a new line" << std::endl;
+    Location loc;
+    Rotation rot;
+    int frame_num = parseLine(line, loc, rot);
 
-      // Convert rotation angles to radians
-      rot.pitch = rot.pitch * M_PI / 180.0;
-      rot.yaw = rot.yaw * M_PI / 180.0;
-      rot.roll = rot.roll * M_PI / 180.0;
 
+    if (frame_num-1 == data_num){
+      // // Convert rotation angles to radians
+      // rot.pitch = rot.pitch * M_PI / 180.0;
+      // rot.yaw = rot.yaw * M_PI / 180.0;
+      // rot.roll = rot.roll * M_PI / 180.0;
+
+      // Defining a rotation matrix and translation vector
       // Calculate translation matrix
-      Eigen::Translation3d translation(loc.x, loc.y, loc.z);
-      // Eigen::Matrix4d translationMatrix = translation.matrix();
+      translation_matrix.block<3, 1>(0, 3) = Eigen::Vector3d(loc.x, loc.y, loc.z);
+      std::cout << "Translation Matrix: \n" << translation_matrix << std::endl;
 
       // Calculate rotation matrix
-      Eigen::AngleAxisd rollAngle(rot.roll, Eigen::Vector3d::UnitX());
-      Eigen::AngleAxisd yawAngle(rot.yaw, Eigen::Vector3d::UnitY());
-      Eigen::AngleAxisd pitchAngle(rot.pitch, Eigen::Vector3d::UnitZ());
-      Eigen::Quaterniond quaternion = rollAngle * yawAngle * pitchAngle;
-      std::cout << quaternion;
-      // Eigen::Matrix4d rotationMatrix = quaternion.toRotationMatrix();
+      // Eigen::AngleAxisd rollAngle(rot.roll, Eigen::Vector3d::UnitX());
+      // std::cout << "Roll Matrix: \n" << rollAngle.matrix() <<std::endl;
+      // Eigen::AngleAxisd yawAngle(rot.yaw, Eigen::Vector3d::UnitY());
+      // std::cout << "Yaw Matrix: \n" <<  yawAngle.matrix() <<std::endl;
 
-      // printMatrix("Translation Matrix", translationMatrix);
-      // printMatrix("Rotation Matrix", rotationMatrix);
+      // Eigen::AngleAxisd pitchAngle(rot.pitch, Eigen::Vector3d::UnitZ());
+      // std::cout << "Pitch Matrix: \n" << pitchAngle.matrix() <<std::endl;
+
+      // Eigen::Quaterniond quaternion = rollAngle * yawAngle * pitchAngle;
+      // std::cout << "Quaternion: \n" << quaternion << std::endl;
+      // Eigen::Matrix3d rotationMatrix = quaternion.toRotationMatrix();
+
+      // Calculate sin and cos of the angles
+      double c1 = std::cos(rot.yaw);
+      double c2 = std::cos(rot.pitch);
+      double c3 = std::cos(rot.roll);
+      double s1 = std::sin(rot.yaw);
+      double s2 = std::sin(rot.pitch);
+      double s3 = std::sin(rot.roll);
+
+      // Create the rotation matrix
+      Eigen::Matrix3d rotationMatrix;
+      rotationMatrix << c1 * c2, c1 * s2 * s3 - s1 * c3, c1 * s2 * c3 + s1 * s3,
+                        s1 * c2, s1 * s2 * s3 + c1 * c3, s1 * s2 * c3 - c1 * s3,
+                        -s2, c2 * s3, c2 * c3;
+
+      // Eigen::Matrix3d rotationMatrix = rollAngle * yawAngle * pitchAngle;
+
+
+      std::cout << "Rotation Matrix: \n" << rotationMatrix << std::endl;
+
+      // combining the two matricies
+      translation_matrix (0, 0 ) = rotationMatrix (0, 0);
+      translation_matrix (0, 1 ) = rotationMatrix (0, 1);
+      translation_matrix (0, 2 ) = rotationMatrix (0, 2);
+      translation_matrix (1, 0 ) = rotationMatrix (1, 0);
+      translation_matrix (1, 1 ) = rotationMatrix (1, 1);
+      translation_matrix (1, 2 ) = rotationMatrix (1, 2);
+      translation_matrix (2, 0 ) = rotationMatrix (2, 0);
+      translation_matrix (2, 1 ) = rotationMatrix (2, 1);
+      translation_matrix (2, 2 ) = rotationMatrix (2, 2);
+      std::cout << "Final Transform Matrix: \n" << translation_matrix << std::endl;
+      return;
+    }
+
   }
+
+  return;
 }
 
 void print4x4Matrix (const Eigen::Matrix4d & matrix)
@@ -216,8 +254,7 @@ const double GPS_NOISE_METERS = 10.0;
 int main (int argc, char* argv[])
 {
 
-  generate_matrix();
-  return (0);
+
 
   // The point clouds we will be using
   PointCloudT::Ptr cloud_i (new PointCloudT);  // infrastructure point cloud <-- we are trying to align to this
@@ -269,17 +306,29 @@ int main (int argc, char* argv[])
   std::cout << "\nLoaded file " << infra_pcd << " (" << cloud_i->size () << " points) in " << time.toc () << " ms\n" << std::endl;
   
 
-  std::vector<Eigen::Matrix4d> poses_i = load_poses(std::string("../../pose.txt"));
+  // std::vector<Eigen::Matrix4d> poses_i = load_poses(std::string("../../pose.txt"));
+  Eigen::Matrix4d c_transform_matrix = Eigen::Matrix4d::Identity();
+  Eigen::Matrix4d i_transform_matrix = Eigen::Matrix4d::Identity();
+  generate_matrix(c_transform_matrix, std::string("../../splits/pose_c.txt"),data_num);
+  generate_matrix(i_transform_matrix, std::string("../../splits/pose_i.txt"),data_num);
+  cout << "c transform matrix:\n" << c_transform_matrix << std::endl;
+  cout << "i transform matrix:\n" << i_transform_matrix << std::endl;
+
+  pcl::transformPointCloud (*cloud_i, *cloud_i, i_transform_matrix);
+  pcl::transformPointCloud (*cloud_c, *cloud_c, c_transform_matrix);
+  *cloud_c_original = *cloud_c; //create two different car point clouds for comparison
+
+
+
 
  //std::vector<Eigen::Matrix4d> poses_i = load_poses(std::string("../../splits/pose_i.txt"));
   //std::cout << poses_i[0];
-  std::cout << "Loaded file " << "poses_i.txt" << " (" << poses_i.size() << " transforms)\n" << std::endl;
-  std::cout << poses_i[0];
+  // std::cout << "Loaded file " << "poses_i.txt" << " (" << poses_i.size() << " transforms)\n" << std::endl;
+  // std::cout << poses_i[0];
 
 // std::vector<Eigen::Matrix4d> poses_c = load_poses(std::string("../../splits/pose_c.txt"));
   //std::cout << "Loaded file " << "poses_c.txt" << " (" << poses_c.size() << " transforms)\n" << std::endl;
   
-  *cloud_c_original = *cloud_c; //create two different car point clouds for comparison
 
   std:cout << "ICP Starting\n";
 

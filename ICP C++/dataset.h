4dd9cb0d847a5cd3975ceead0037531d061ad3cf
Ben Giacalone <24901846+Boxxfish@users.noreply.github.com>
2024-05-01 16:22:48 -0400

    Refactor run (#1)
    * Refactor dataset
    
    * Minor ds changes
    
    * Remove frame from build
    
    * Refactor metrics
    
    * Refactor pipeline
    
    * Fix errors during refactor
    
    * Prevent segfaults

--- ICP C++/dataset.h
+++ ICP C++/dataset.h
@@ -7,24 +7,45 @@
 
 #include <string>
 #include <Eigen/Geometry> // Include this for additional Eigen functionalities
-#include "frame.h"
 
-class Frame;
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/registration/icp.h>
+typedef pcl::PointXYZ PointT;
+typedef pcl::PointCloud<PointT> PointCloudT;
 
-class Dataset{
-    private:
-        void load_i_pose();
-        void load_c_poses();
-    public:
-        std::string path;
-        Eigen::Matrix4d i_pose;
-        std::vector<Eigen::Matrix4d> c_poses;
-        std::vector<Eigen::Matrix4d> c_poses_corrected;
-        std::vector<double> computation_time_list;
-
-        Dataset();
-        Dataset(std::string& path); //load ups the poses
-        Frame getFrame(int frame_name);
+/// A frame of data.
+struct Frame
+{
+    /// Point cloud for infra.
+    PointCloudT::Ptr cloud_i;
+    /// Point cloud for vehicle.
+    PointCloudT::Ptr cloud_c;
+    /// Vehicle ground truth pose.
+    Eigen::Matrix4d pose_c;
 };
+
+/// A datset containing all information across frames.
+class Dataset
+{
+public:
+    /// Infra ground truth pose.
+    Eigen::Matrix4d i_pose;
+    /// Frames in our dataset, should be of length 300.
+    std::vector<Frame> frames;
+    /// Indices corresponding to easy frames (overlap >= 50%).
+    std::vector<int> easy_idxs;
+    /// Indices corresponding to hard frames (overlap < 50%).
+    std::vector<int> hard_idxs;
+
+    /// Loads poses and frames from a path.
+    Dataset(const std::string &path);
+};
+
+/// Reads a list of poses from a file.
+std::vector<Eigen::Matrix4d> read_poses(const std::string &path);
+
+/// Loads a point cloud from a file.
+PointCloudT::Ptr load_pc(const std::string &path);
 
 #endif
--- ICP C++/main.cpp
+++ ICP C++/main.cpp
@@ -11,57 +11,41 @@
 #include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
 #include <pcl/visualization/pcl_visualizer.h>
-#include <pcl/console/time.h>   // TicToc
+#include <pcl/console/time.h> // TicToc
 typedef pcl::PointXYZ PointT;
 typedef pcl::PointCloud<PointT> PointCloudT;
 #include <Eigen/Geometry> // Include this for additional Eigen functionalities
 
 #include "visualizer.h"
 #include "dataset.h"
-#include "frame.h"
 #include "pipeline.h"
 #include "metrics.h"
 #include <pcl/console/time.h> // TicToc
 
-#include "json.hpp"
-using json = nlohmann::json;
-
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 
-
-    //ALIGNMENT
-    // For reproducability
+    // ALIGNMENT
+    //  For reproducability
     srand(100);
 
     std::string path = "../../data/Dataset_1/D1/";
 
-    std::ifstream f(path + std::string("splits/info.json"));
-    auto data = json::parse(f);
-    std::vector<int> hard_idxs = data["hard"];
-    std::vector<int> easy_idxs = data["easy"];
+    Dataset dataset(path);
 
-
-    Dataset dataset1_d1(path);
     std::cout << "Dataset Loaded from " << path << std::endl;
-    std::vector<double> time_list;
-
-    std::cout << "Dataset Size: " << dataset1_d1.c_poses.size() <<std::endl;
+    std::cout << "Dataset Size: " << dataset.frames.size() << std::endl;
 
     // // SINGLE FRAME RUN FOR TESTING
     // SimplePipeline pipeline;
     // Frame frame = dataset1_d1.getFrame(69);
     // Eigen::Matrix4d transform = pipeline.guess_v_pose(frame);
     // //vizulization stuff
-    // PointCloudT::Ptr cloud_c_new (new PointCloudT); 
+    // PointCloudT::Ptr cloud_c_new (new PointCloudT);
     // pcl::transformPointCloud (*frame.cloud_c, *cloud_c_new, transform);
-    // create_visualizer(std::string("Demo Visualizer"), frame.cloud_i, frame.cloud_c, cloud_c_new); 
+    // create_visualizer(std::string("Demo Visualizer"), frame.cloud_i, frame.cloud_c, cloud_c_new);
 
-
-    //READ FULL DATASET RUN
-    SimplePipeline pipeline(dataset1_d1);
-    pipeline.run();
-
-    //METRICS
-    print_metrics(pipeline, easy_idxs, hard_idxs);
+    // METRICS
+    SimplePipeline pipeline;
+    print_metrics(pipeline, dataset);
 }
